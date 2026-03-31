5fa8bd4ee3949f96109b726a4f1d28b8f53fcaf9
RaaphaelGomesS <rafagomes1706@gmail.com>
2026-03-31 17:52:36 -0300

    test: add integration tests for consents controller

--- src/test/java/com/sensedia/sample/consents/TestConsentsApplication.java
+++ src/test/java/com/sensedia/sample/consents/TestConsentsApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 public class TestConsentsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(ConsentsApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(ConsentsApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
 }
